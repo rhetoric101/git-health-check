d1b771df84c657fe0652342bdb6b11add58d4209
Rob Siebens <robert@robertsiebens.com>
2020-09-07 22:44:12 -0700

    I clarified that the batch file is for Windows.

--- git_check.bat
+++ git_check.bat
@@ -1,5 +1,5 @@
 echo off
-title batch script to execute GIT Bash diagnostic script.
+title batch script to execute GIT Bash diagnostic script on Windows.
 
 
 "%ProgramFiles%\Git\git-bash.exe" git_check_10.bash
